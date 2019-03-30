adec58de2df195b7619dbde680813595e9d01f4c
Jessy Sidhu <jks46@lib3000-icmac28.ucslabs.sfu.ca>
2019-03-30 14:09:24 -0700

    Initial Commit

---
+++ iSpy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSpy
+//
+//  Created by Jessy Sidhu on 2019-03-30.
+//  Copyright © 2019 Allison and Jessy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
